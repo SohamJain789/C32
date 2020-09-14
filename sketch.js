92ab4e2b921296dfebcd4e7619c72946e4ba7aac
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-09-14 19:00:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,119 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+var bg
+var gameState = "onSling";
+var score = 0;
+function preload() {
+    //Load the image in the computer memory 
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    //Display the backgroundIMG
+    if(backgroundImg){
+    background(backgroundImg);
+    }
+   
+   noStroke();
+   textSize(35)
+   fill("white");
+   text("score:"+score,width-300,50)
+
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+       // slingshot.attach(bird.body);
+    }
+}
+//asynchronous function == waits for a few lines to compelete it's exectution then goes to the next line 
+//normally the functions are synchronouas in java script 
+async function getBackgroundImg(){
+    //fetch sends request to the API(Application Proggraming Interface) and collects the response
+    //API request take some extra time so we write await in front of fetch and hence function becomes asynchronous function    
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    //Convert the response from api to json(JavaScript Object Notation) format == {"attribute name":"attribute value",,,,,} 
+    var responseJson = await response.json();
+    //We only want date and time from the response from the entire JSON response 
+    var dateTime = responseJson.datetime;
+    //cut ony the hour from date and time 
+    var hour = dateTime.slice(11,13);
+    //if it is morning 6to evening7,get the morning Img else get the night Img
+    if(hour>=06 && hour<=19){
+    bg ="sprites/bg.png";
+    }
+    else{
+     bg ="sprites/bg2.jpg";
+    }
+    backgroundImg=loadImage(bg)    
+}
